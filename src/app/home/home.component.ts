41ec7ca9b896fbf29777af2fd7e5ae81e16f78ff
Minh Chuong <ch31052k@gmail.com>
2022-11-03 09:20:03 +0700

    ngFor

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,18 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  public name = 'Chương';
-  public age = 23;
-  constructor() { }
+  name = 'Chương';
+  age = 23;
+  fruits = ['Apple', 'Orange', 'Banana'];
+  constructor() {}
 
   ngOnInit(): void {
-  }
-
-  public resetName():void {
-    console.log('resetName');
-    this.name = '';
+    console.log('fruits = ', this.fruits);
   }
 }
